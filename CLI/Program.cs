eb68b4a0243028fbea2579e553e7282d1e4f88af
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-12-11 08:46:06 +0330

    CLI

---
+++ CLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using ParsiCoin;
+using System;
+using System.Diagnostics;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Stopwatch();
+            s.Start();
+            var m = new MerkleTree();
+            s.Stop();
+            Console.WriteLine(s.Elapsed);
+            Console.WriteLine(m.Root);
+            Console.ReadKey();
+        }
+    }
+}
